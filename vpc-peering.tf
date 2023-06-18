16092216ba3ab6ae7f3133e0560513e945ee0edd
Zomb <awsrameshraju@gmail.com>
2023-06-18 13:05:17 +1000

    removed vpc peering

--- vpc-peering.tf
+++ vpc-peering.tf
@@ -1,26 +1,26 @@
-data "aws_vpc" "ansible_vpc" {
-  id = "vpc-0f8d4253c7226aa6a"
-}
+# data "aws_vpc" "ansible_vpc" {
+#   id = "vpc-0f8d4253c7226aa6a"
+# }
 
-data "aws_route_table" "ansible_vpc_rt" {
-  subnet_id = "subnet-00f23dc50a2b98663" 
-}
+# data "aws_route_table" "ansible_vpc_rt" {
+#   subnet_id = "subnet-00f23dc50a2b98663" 
+# }
 
-resource "aws_vpc_peering_connection" "foo" {
-  peer_vpc_id   = data.aws_vpc.ansible_vpc.id
-  vpc_id        = aws_vpc.default.id
-  auto_accept = true
-  accepter {
-    allow_remote_vpc_dns_resolution = true
-  }
+# resource "aws_vpc_peering_connection" "foo" {
+#   peer_vpc_id   = data.aws_vpc.ansible_vpc.id
+#   vpc_id        = aws_vpc.default.id
+#   auto_accept = true
+#   accepter {
+#     allow_remote_vpc_dns_resolution = true
+#   }
 
-  requester {
-    allow_remote_vpc_dns_resolution = true
-  }
-  tags = {
-  Name        = "Ansible-${var.vpc_name}-Peering"
-  }
-}
+#   requester {
+#     allow_remote_vpc_dns_resolution = true
+#   }
+#   tags = {
+#   Name        = "Ansible-${var.vpc_name}-Peering"
+#   }
+# }
 
 # resource "aws_route" "peering" {
 #   route_table_id            = aws_route_table.terraform-public.id
